32b6e08875d29031637ca707e53b628cef5b3272
DOOJ SAHU <doojsahu@DOOJs-MacBook-Pro.local>
2021-09-06 23:58:08 +0530

    onclick handler

--- src/Login.js
+++ src/Login.js
@@ -1,38 +1,66 @@
-
-import React from 'react';
-import { Redirect, useHistory } from 'react-router-dom';
-import './App.scss';
-import Dashboard from './components/Dashboard';
-
+import React, { useState } from "react";
+import { Redirect, useHistory } from "react-router-dom";
+import "./App.scss";
+import Dashboard from "./components/Dashboard";
 
 function App() {
-        let history = useHistory();
+    let history = useHistory();
 
-        const redirect = () => {
-          history.push('/dashboard')
+    // const redirect = () => {
+    //   history.push('/dashboard')
+    // }
+
+    const [inputs, setInputs] = useState({
+        uid: "",
+        pw: "",
+    });
+    const changeHandle = (e) => {
+        setInputs({
+            ...inputs,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const submitHanlder = (e) => {
+        e.preventDefault();
+        if (inputs.uid && inputs.pw != "") {
+            console.log(inputs);
+            history.push("/dashboard");
         }
+    };
 
-
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-              <h3 className='text-center mb-5 mt-5'>Login</h3>
-          </div>
-          <div class="mb-3">
-            <label class="form-label">User Name</label>
-            <input type="text" class="form-control" placeholder="username/id" />
-          </div>
-          <div class="mb-3">
-            <label class="form-label">Password</label>
-            <input type="password" class="form-control" placeholder="Enter Password" />
-          </div>
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h3 className="text-center mb-5 mt-5">Login</h3>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">User Name</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="username/id"
+                            name="uid"
+                            // value={loginId}
+                            // onChange={(e) => {setLoginId(e.target.value)}}
+                            value={inputs.name}
+                            onChange={changeHandle}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Password</label>
+                        <input type="password" className="form-control" placeholder="Enter Password" name="pw" value={inputs.pw} onChange={changeHandle} />
+                    </div>
+                </div>
+                {/* <button type="button" className="btn btn-primary" onClick={redirect}>Login</button> */}
+                <button type="button" className="btn btn-primary" onClick={submitHanlder}>
+                    Login
+                </button>
+            </div>
         </div>
-        <button type="button" className="btn btn-primary" onClick={redirect}>Login</button>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
